fb148f313b652503a0449b62c88896e2ad643d14
Carlos Eduardo de Souza Furtunato <carlos.esf@aluno.ifsc.edu.br>
2024-09-30 15:21:37 -0300

    Calculadora IMC

--- ClaculadoraIMC/app/src/main/java/com/example/claculadoraimc/MainActivity.java
+++ ClaculadoraIMC/app/src/main/java/com/example/claculadoraimc/MainActivity.java
@@ -2,67 +2,57 @@ package com.example.claculadoraimc;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.ImageView;
-import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-    //Declarando variaveis para usar no metodo//
-    EditText editTextAltura;
-    EditText editTextPeso;
-    TextView TextViewResultado;
-    Button button;
-    //ImageView ImageView;
 
-    @Override
+    Button button;
+    EditText editTextNome;
+    EditText editTextPeso ;
+    EditText editTextAltura;
+    TextView lbl;
+
+    float imc;
+    float peso;
+    float altura;
+    String nome;
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-
-        //"linkando ações"
-        editTextPeso = findViewById(R.id.editTextPeso);
-        editTextAltura = findViewById(R.id.editTextAltura);
-        TextViewResultado = findViewById(R.id.TextViewResultado);
-        //ImageView = findViewById(R.id.imageView);
-
-        Button button = (Button) findViewById(R.id.button);
-
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-                //passando os dados para Float
-                Float peso = Float.parseFloat(editTextPeso.getText().toString());
-                Float altura = Float.parseFloat(editTextAltura.getText().toString());
-
-                //calculando resultado (imc = peso/(altura+altura))
-                Float imc = peso / (altura * altura);
-                //TextViewResultado.setText(String.valueOf(imc));
-
-                //irParaSegundaTela()
-
-                //if (imc < 18.5){
-                //ImageView.setImageResource(R.drawable.abaixopeso);
-                //}
-                Intent intent = new Intent(getApplicationContext(),TelaDois.class);
-                startActivity(intent);
-            }
-        });
-
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
 
+                editTextPeso = findViewById(R.id.editTextPeso);
+                editTextAltura = findViewById(R.id.editTextAltura);
+
+                peso = Float.parseFloat(String.valueOf(editTextPeso.getText()));
+                altura = Float.parseFloat(String.valueOf(editTextAltura.getText()));
+                imc = (peso)/(altura*altura);
+
+                Intent i = new Intent(getApplicationContext(),TelaDois.class);
+                i.putExtra("peso",peso);
+                i.putExtra("altura",altura);
+                i.putExtra("imc",imc);
+                startActivity(i);
+
+            }
+        });
     }
 }
