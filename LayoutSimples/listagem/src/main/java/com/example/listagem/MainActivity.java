0c3a913f82e95023bb38688aad701caa39c5445a
Carlos Eduardo de Souza Furtunato <carlos.esf@aluno.ifsc.edu.br>
2024-10-07 10:44:12 -0300

    Mostrar numero no Toast

--- LayoutSimples/listagem/src/main/java/com/example/listagem/MainActivity.java
+++ LayoutSimples/listagem/src/main/java/com/example/listagem/MainActivity.java
@@ -3,8 +3,11 @@ package com.example.listagem;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     String[]  listagem = new String[] {"","Aala","Ford","Fiat"};
@@ -19,5 +22,12 @@ public class MainActivity extends AppCompatActivity {
         listView = findViewById(R.id.listView);
         ArrayAdapter arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, android.R.id.text1,listagem);
         listView.setAdapter(arrayAdapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Toast.makeText(MainActivity.this, Integer.toString(i),Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
