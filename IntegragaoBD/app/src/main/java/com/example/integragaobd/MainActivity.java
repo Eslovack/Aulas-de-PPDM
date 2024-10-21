b674484739618c282a4cebec46632ffe7e9f0882
Carlos Eduardo de Souza Furtunato <carlos.esf@aluno.ifsc.edu.br>
2024-10-21 10:22:06 -0300

    Banco integrado e registrando dados

--- IntegragaoBD/app/src/main/java/com/example/integragaobd/MainActivity.java
+++ IntegragaoBD/app/src/main/java/com/example/integragaobd/MainActivity.java
@@ -2,14 +2,19 @@ package com.example.integragaobd;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ContentValues;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     SQLiteDatabase romulo;
     EditText editNome, editDataNsc, editEmail;
+    Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,13 +22,32 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         editNome = findViewById(R.id.editNome);
-        editEmail= findViewById(R.id.editEmail);
+        editEmail = findViewById(R.id.editEmail);
         editDataNsc = findViewById(R.id.editDataNsc);
+        button = findViewById(R.id.button);
 
-        romulo = openOrCreateDatabase("meu db",MODE_PRIVATE, null);
-        romulo.execSQL("CREATE TABLE pessoas (id int primary key autoincrement,nome varchar, email varchar, dtnsc date)");
+        romulo = openOrCreateDatabase("meu db", MODE_PRIVATE, null);
+        romulo.execSQL("CREATE TABLE IF NOT EXISTS pessoas (id INTEGER PRIMARY KEY,nome varchar, email varchar, dtnsc date)");
 
+        //Romulo
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
 
+                String nome = editNome.getText().toString();
+                String email = editEmail.getText().toString();
+                ContentValues cv = new ContentValues();
+                cv.put("nome",nome);
+                cv.put("email",email);
+
+                long status = romulo.insert("pessoas", null,cv);
+                if (status > 0){
+                    Toast.makeText(getApplicationContext(),"Usuario inserido", Toast.LENGTH_LONG).show();
+                } else{
+                    Toast.makeText(getApplicationContext(),"erro",Toast.LENGTH_LONG).show();
+                }
+            }
+        });
 
     }
 }
